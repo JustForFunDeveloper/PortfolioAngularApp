a17d5458db9c65c3a8ee138f2d2f37bac3b95511
JustForFunDeveloper <tappler.andreas@gmail.com>
2020-04-25 14:24:27 +0200

    * installed store-devtools for ngrx browser support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
-import {SharedModule} from './shared/shared.module';
 
 import {HeaderComponent} from './header/header.component';
 
 import {AuthInterceptorService} from './auth/services/auth-interceptor.service';
 import {StoreModule} from '@ngrx/store';
+import {SharedModule} from './shared/shared.module';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import {AuthEffects} from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule
   ],
   providers: [
