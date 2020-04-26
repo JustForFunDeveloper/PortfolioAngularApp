1976c0a64bf072a37bfd16f3811955fa393380b7
JustForFunDeveloper <tappler.andreas@gmail.com>
2020-04-26 15:56:39 +0200

    * Added version in the environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  fireBaseAPIKey: 'AIzaSyBCTXsSVfQXF85m9R8I4f9N2u5fwZ2-rdk'
+  fireBaseAPIKey: 'AIzaSyBCTXsSVfQXF85m9R8I4f9N2u5fwZ2-rdk',
+  version: '0.0.1'
 };
